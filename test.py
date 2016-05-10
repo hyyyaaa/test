126a7835ab1d5365a952f979b6dceca1f0ef055a
bugrahan <bugrahansahin@gmail.com>
2016-05-10 15:05:16 +0300

    asdas

--- test.py
+++ test.py
@@ -16,3 +16,7 @@ class SimpleTest(unittest.TestCase):
 
     def test_fail(self):
         self.assertEqual(11, 7 + 3)
+
+if __name__ == '__main__':
+    import xmlrunner
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
